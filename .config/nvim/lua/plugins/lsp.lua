c250b20a4ffe8fe251722599365ae7f5a962b314
yaotutu <957810668@qq.com>
2023-07-30 21:00:29 +0800

    添加markdown

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,48 +1,48 @@
 return {
-    {
-        "williamboman/mason-lspconfig.nvim",
+  {
+    "williamboman/mason-lspconfig.nvim",
+  },
+  {
+    "williamboman/mason.nvim",
+  },
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp",
     },
-    {
-        "williamboman/mason.nvim"
-    },
-    {
-        "neovim/nvim-lspconfig",
-        event = { "BufReadPre", "BufNewFile" },
-        dependencies = {
-            "mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-            "hrsh7th/cmp-nvim-lsp",
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup()
+      require("lspconfig").lua_ls.setup({
+        settings = {
+          Lua = {
+            diagnostics = {
+              -- Get the language server to recognize the `vim` global
+              globals = { "vim" },
+            },
+          },
         },
-        config = function()
-            require("mason").setup()
-            require("mason-lspconfig").setup()
-            require('lspconfig').lua_ls.setup {
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            -- Get the language server to recognize the `vim` global
-                            globals = { 'vim' },
-                        },
-                    },
-                },
-            }
-            require('lspconfig').tsserver.setup {
-                filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', }
-            }
-            require('lspconfig').tailwindcss.setup {
-                filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
-            }
-            require('lspconfig').cssls.setup {}
-            require 'lspconfig'.volar.setup {
-                filetypes = { 'vue' }
-            }
-            require 'lspconfig'.emmet_language_server.setup {
-                filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
-            }
+      })
+      require('lspconfig').tsserver.setup {
+        filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', }
+      }
+      require("lspconfig").tailwindcss.setup({
+        filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+      })
+      require("lspconfig").cssls.setup({})
+      require("lspconfig").volar.setup({
+        filetypes = { "vue" },
+      })
+      require("lspconfig").emmet_language_server.setup({
+        filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+      })
+      require("lspconfig").marksman.setup({})
 
 
-            -- require 'lspconfig'.unocss.setup {}
-        end
-    },
-
+      -- require 'lspconfig'.unocss.setup {}
+    end,
+  },
 }
