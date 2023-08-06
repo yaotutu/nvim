a8511b50620d7ca6c51ea82116a6771e31d15f8b
yaotutu <957810668@qq.com>
2023-08-06 22:46:54 +0800

    update vim

--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -11,12 +11,26 @@ return {
       -- Set the log level
       log_level = vim.log.levels.WARN,
       -- All formatter configurations are opt-in
+      on_post_format = function(data)
+        -- data包含格式化的文件路径和耗时
+        vim.notify("Formatted " .. data.file .. " in " .. data.duration .. " ms")
+      end,
+
       filetype = {
         lua = {
           require("formatter.filetypes.lua").stylua,
         },
+        javascript = {
+          require("formatter.filetypes.typescript").prettier,
+        },
+        javascriptreact = {
+          require("formatter.filetypes.typescript").prettier,
+        },
         typescript = {
-          require("formatter.filetypes.lua").prettier,
+          require("formatter.filetypes.typescript").prettier,
+        },
+        typescriptreact = {
+          require("formatter.filetypes.typescript").prettier,
         },
         ["*"] = {
           require("formatter.filetypes.any").remove_trailing_whitespace,
