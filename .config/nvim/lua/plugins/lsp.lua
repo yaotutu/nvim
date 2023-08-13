1fb688e20182342bff2f3fd66cc9eec296093c81
yaotutu <957810668@qq.com>
2023-08-13 23:30:58 +0800

    add config for vue

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -26,23 +26,55 @@ return {
           },
         },
       })
-      require('lspconfig').tsserver.setup {
-        filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', }
-      }
+      require("lspconfig").tsserver.setup({
+        filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact" },
+      })
       require("lspconfig").tailwindcss.setup({
         filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
       })
       require("lspconfig").cssls.setup({})
       require("lspconfig").volar.setup({
-        filetypes = { "vue" },
+        filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+        init_options = {
+          takeOverMode = {
+            module = "js",
+            filetypes = { "vue" },
+          },
+        },
       })
       require("lspconfig").emmet_language_server.setup({
         filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
       })
       require("lspconfig").marksman.setup({})
 
-
       -- require 'lspconfig'.unocss.setup {}
+      local cwd = vim.fn.getcwd()
+      local util = require("lspconfig.util")
+      local project_root = util.find_node_modules_ancestor(cwd)
+
+      local vue_path = util.path.join(project_root, "node_modules", "vue")
+      local is_vue = vim.fn.isdirectory(vue_path) == 1
+      if is_vue then
+        require("lspconfig").volar.setup({
+          filetypes = {
+            "vue",
+            "javascript",
+            "javascript.jsx",
+            "typescript",
+            "typescript.tsx",
+            "javascriptreact",
+            "typescriptreact",
+            "json",
+          },
+        })
+        require("lspconfig").tsserver.setup({
+          autostart = false,
+          root_dir = function()
+            return false
+          end,
+          single_file_support = false,
+        })
+      end
     end,
   },
 }
