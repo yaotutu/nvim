1048e27c7011041a2567621c5861f6fb2cf89016
yaotutu <957810668@qq.com>
2023-08-17 17:20:45 +0800

    update viim

---
+++ .config/nvim/lua/plugins/format.lua
@@ -0,0 +1,11 @@
+return {
+  "nvimdev/guard.nvim",
+  config = function()
+    require('guard').setup({
+      -- the only options for the setup function
+      fmt_on_save = true,
+      -- Use lsp if no formatter was defined for this filetype
+      lsp_as_default_formatter = true,
+    })
+  end
+}
