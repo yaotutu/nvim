ef1480ce351e28d509da90706105abba4c4c2d6e
yaotutu <957810668@qq.com>
2023-08-28 20:49:22 +0800

    update vim

--- .config/nvim/lua/util/init.lua
+++ .config/nvim/lua/util/init.lua
@@ -1,36 +1,6 @@
-local Util = require("lazy.core.util")
-
 local M = {}
 
 M.root_patterns = { ".git", "lua" }
-
----@param on_attach fun(client, buffer)
-function M.on_attach(on_attach)
-    vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-            local buffer = args.buf
-            local client = vim.lsp.get_client_by_id(args.data.client_id)
-            on_attach(client, buffer)
-        end,
-    })
-end
-
-function M.fg(name)
-    ---@type {foreground?:number}?
-    local hl = vim.api.nvim_get_hl and vim.api.nvim_get_hl(0, { name = name }) or vim.api.nvim_get_hl_by_name(name, true)
-    local fg = hl and hl.fg or hl.foreground
-    return fg and { fg = string.format("#%06x", fg) }
-end
-
----@param fn fun()
-function M.on_very_lazy(fn)
-    vim.api.nvim_create_autocmd("User", {
-        pattern = "VeryLazy",
-        callback = function()
-            fn()
-        end,
-    })
-end
 
 -- returns the root directory based on:
 -- * lsp workspace folders
