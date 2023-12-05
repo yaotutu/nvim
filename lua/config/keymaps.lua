c96a96f6c4c522228bff32699750959712203032
yaotutu <957810668@qq.com>
2023-12-05 20:50:31 +0800

    add project plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,8 +15,8 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 local opt = {
-    noremap = true,
-    silent = true,
+  noremap = true,
+  silent = true,
 }
 
 -- 本地变量
@@ -142,43 +142,43 @@ map("v", "<C-_>", "gcc", { noremap = false })
 
 -- lsp 快捷键设置
 vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-    callback = function(ev)
-        -- Enable completion triggered by <c-x><c-o>
-        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-        -- Buffer local mappings.
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
-        local opts = { buffer = ev.buf }
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set('n', 'gh', vim.lsp.buf.hover, opts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-        vim.keymap.set('n', 'gk', vim.lsp.buf.signature_help, opts)
-        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-        vim.keymap.set('n', '<space>wl', function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, opts)
-        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-        vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', '<space>f', function()
-            vim.lsp.buf.format { async = true }
-        end, opts)
-    end,
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gh', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', 'gk', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
 })
 vim.keymap.set("n", "<leader>nl", function()
-    require("noice").cmd("last")
+  require("noice").cmd("last")
 end)
 vim.keymap.set("n", "<leader>nl", function()
-    require("noice").cmd("dismiss")
+  require("noice").cmd("dismiss")
 end)
 vim.keymap.set("n", "<leader>nh", function()
-    require("noice").cmd("history")
+  require("noice").cmd("history")
 end)
 vim.keymap.set("n", "<leader>nt", function()
-    require("noice").cmd("telescope")
+  require("noice").cmd("telescope")
 end)
 
 -- SnipRun
@@ -196,16 +196,16 @@ vim.keymap.set("n", "gR", function() require("trouble").open("lsp_references") e
 
 -- spectre
 vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
-    desc = "Toggle Spectre"
+  desc = "Toggle Spectre"
 })
 vim.keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
-    desc = "Search current word"
+  desc = "Search current word"
 })
 vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
-    desc = "Search current word"
+  desc = "Search current word"
 })
 vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
-    desc = "Search on current file"
+  desc = "Search on current file"
 })
 
 -- telescope
@@ -215,6 +215,7 @@ vim.keymap.set('n', '<leader>ff', Util.telescope("files"), opt)
 vim.keymap.set('n', '<leader>fF', Util.telescope("files", { cwd = false }), opt)
 vim.keymap.set('n', '<leader>fr', "<cmd>Telescope oldfiles<cr>", opt)
 vim.keymap.set('n', '<leader>fR', Util.telescope("oldfiles", { cwd = vim.loop.cwd() }), opt)
+vim.keymap.set('n', '<leader>fp', "<cmd>Telescope projects<cr>", opt)
 -- git
 vim.keymap.set('n', '<leader>gc', "<cmd>Telescope git_commits<cr>", opt)
 vim.keymap.set('n', '<leader>gs', "<cmd>Telescope git_status<cr>", opt)
@@ -227,22 +228,22 @@ vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
 vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
 vim.keymap.set('n', '<Leader>B', function() require('dap').set_breakpoint() end)
 vim.keymap.set('n', '<Leader>lp',
-    function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+  function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
 vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
 vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
 vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
-    require('dap.ui.widgets').hover()
+  require('dap.ui.widgets').hover()
 end)
 vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
-    require('dap.ui.widgets').preview()
+  require('dap.ui.widgets').preview()
 end)
 vim.keymap.set('n', '<Leader>df', function()
-    local widgets = require('dap.ui.widgets')
-    widgets.centered_float(widgets.frames)
+  local widgets = require('dap.ui.widgets')
+  widgets.centered_float(widgets.frames)
 end)
 vim.keymap.set('n', '<Leader>ds', function()
-    local widgets = require('dap.ui.widgets')
-    widgets.centered_float(widgets.scopes)
+  local widgets = require('dap.ui.widgets')
+  widgets.centered_float(widgets.scopes)
 end)
 
 
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,29 +1,30 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    cmd = "Telescope",
-    config = function()
-        require("telescope").setup({
-            defaults = {
-                -- vertical , center , cursor
-                layout_strategy = "vertical",
-                mappings = {
-                    i = {
-                        -- 上下移动
-                        ["<C-j>"] = "move_selection_next",
-                        ["<C-k>"] = "move_selection_previous",
-                        -- 历史记录
-                        -- ["<C-n>"] = "cycle_history_next",
-                        -- ["<C-p>"] = "cycle_history_prev",
-                        -- 关闭窗口
-                        ["<Esc"] = "close",
-                        -- 预览窗口上下滚动
-                        ["<C-u>"] = "preview_scrolling_up",
-                        ["<C-d>"] = "preview_scrolling_down",
-                    },
-                }
-            },
-        })
-        require("telescope").load_extension("flutter")
-    end
+  'nvim-telescope/telescope.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  cmd = "Telescope",
+  config = function()
+    require("telescope").setup({
+      defaults = {
+        -- vertical , center , cursor
+        layout_strategy = "vertical",
+        mappings = {
+          i = {
+            -- 上下移动
+            ["<C-j>"] = "move_selection_next",
+            ["<C-k>"] = "move_selection_previous",
+            -- 历史记录
+            -- ["<C-n>"] = "cycle_history_next",
+            -- ["<C-p>"] = "cycle_history_prev",
+            -- 关闭窗口
+            ["<Esc"] = "close",
+            -- 预览窗口上下滚动
+            ["<C-u>"] = "preview_scrolling_up",
+            ["<C-d>"] = "preview_scrolling_down",
+          },
+        }
+      },
+    })
+    require("telescope").load_extension("flutter")
+    require('telescope').load_extension('projects')
+  end
 }
