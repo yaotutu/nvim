168e9b9a8d23db1675ec96647416aa3cae16c165
yaotutu <957810668@qq.com>
2023-12-21 09:26:17 +0000

    update lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -14,7 +14,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
           require("mason-lspconfig").setup {
-            ensure_installed = { "lua_ls", "eslint" },
+            ensure_installed = { "lua_ls", "eslint", "cssls", "emmet_ls", "volar", "marksman", "tailwindcss"},
           }
         end
       },
