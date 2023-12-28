5f8ea247ddc8506e24e6461668df81eab14633b4
yaotutu <957810668@qq.com>
2023-12-28 17:14:53 +0800

    update flutter tools

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -30,8 +30,8 @@ return {
             },
             dev_log = {
                 enabled = true,
-                notify_errors = false, -- if there is an error whilst running then notify the user
-                open_cmd = "sp",       -- command to use to open the log buffer
+                notify_errors = true, -- if there is an error whilst running then notify the user
+                open_cmd = "tabedit",       -- command to use to open the log buffer
             },
         }
     end
