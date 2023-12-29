c1566ac79aa6137e7c6d98c3c681871d2af702c7
yaotutu <957810668@qq.com>
2023-12-29 13:37:31 +0800

    update formatting

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,16 @@
+return {
+  "stevearc/conform.nvim",
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        javascript = { { "prettierd", "prettier" } },
+      },
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    })
+  end,
+}
