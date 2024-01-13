112d7b772e6b428e27a7b7dde4d583a3552f277f
yaotutu <957810668@qq.com>
2024-01-13 16:01:00 +0800

    add fancy plugin
    Add macro status on lualine to resolve the issue of CMP not working.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,16 +1,19 @@
 local gstatus = { ahead = 0, behind = 0 }
 local function update_gstatus()
-  local Job = require 'plenary.job'
+  local Job = require("plenary.job")
   Job:new({
-    command = 'git',
-    args = { 'rev-list', '--left-right', '--count', 'HEAD...@{upstream}' },
+    command = "git",
+    args = { "rev-list", "--left-right", "--count", "HEAD...@{upstream}" },
     on_exit = function(job, _)
       local res = job:result()[1]
-      if type(res) ~= 'string' then
-        gstatus = { ahead = 0, behind = 0 }; return
+      if type(res) ~= "string" then
+        gstatus = { ahead = 0, behind = 0 }
+        return
       end
       local ok, ahead, behind = pcall(string.match, res, "(%d+)%s*(%d+)")
-      if not ok then ahead, behind = 0, 0 end
+      if not ok then
+        ahead, behind = 0, 0
+      end
       gstatus = { ahead = ahead, behind = behind }
     end,
   }):start()
@@ -23,13 +26,13 @@ else
 end
 _G.Gstatus_timer:start(0, 2000, vim.schedule_wrap(update_gstatus))
 
-
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = {
-    { 'dokwork/lualine-ex' },
-    { 'nvim-lua/plenary.nvim' },
-    { 'kyazdani42/nvim-web-devicons' },
+    { "dokwork/lualine-ex" },
+    { "nvim-lua/plenary.nvim" },
+    { "kyazdani42/nvim-web-devicons" },
+    "meuter/lualine-so-fancy.nvim",
   },
   event = "VeryLazy",
   opts = function()
@@ -41,15 +44,25 @@ return {
       },
       extensions = { "nvim-tree", "toggleterm" },
       sections = {
-        lualine_b = { 'ex.git.branch', { function()
-          return gstatus.ahead .. ' ' .. gstatus.behind ..
-              ''
-        end }, 'diff', 'diagnostics' },
+        lualine_a = {
+          { "fancy_mode", width = 3 },
+        },
+        lualine_b = {
+          "ex.git.branch",
+          {
+            function()
+              return gstatus.ahead .. " " .. gstatus.behind .. ""
+            end,
+          },
+          "diff",
+          "diagnostics",
+          "fancy_macro",
+        },
         lualine_c = {
           {
             "lsp_progress",
             spinner_symbols = { " ", " ", " ", " ", " ", " " },
-          }
+          },
         },
         lualine_x = {
           "filesize",
