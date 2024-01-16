84776139e674340812a78cf30da6c06e6ae19e6b
yaotutu <957810668@qq.com>
2024-01-16 17:25:30 +0800

    add typescript inly hints

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,4 @@
 return {
-
   {
     "williamboman/mason.nvim",
     opts = {},
@@ -49,7 +48,29 @@ return {
     config = function()
       local projectType = require("util.tools").detectProjectType()
       if projectType ~= "vue" then
-        require("typescript-tools").setup({})
+        require("typescript-tools").setup({
+          on_attach = function(client, bufnr)
+            client.server_capabilities.documentFormattingProvider = false
+            client.server_capabilities.documentRangeFormattingProvider = false
+
+            if vim.lsp.inlay_hint then
+              vim.lsp.inlay_hint.enable(bufnr, true)
+            end
+          end,
+          settings = {
+            tsserver_file_preferences = {
+              -- Inlay Hints
+              includeInlayParameterNameHints = "all",
+              includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            },
+          },
+        })
       end
     end,
   },
