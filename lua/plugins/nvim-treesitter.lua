ae353b586d0bc11074e6d29545fb5fcc850d3711
yaotutu <957810668@qq.com>
2024-04-27 20:17:43 +0800

    update

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,67 +1,120 @@
+local handler = function(virtText, lnum, endLnum, width, truncate)
+  local newVirtText = {}
+  local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+  local sufWidth = vim.fn.strdisplaywidth(suffix)
+  local targetWidth = width - sufWidth
+  local curWidth = 0
+  for _, chunk in ipairs(virtText) do
+    local chunkText = chunk[1]
+    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+    if targetWidth > curWidth + chunkWidth then
+      table.insert(newVirtText, chunk)
+    else
+      chunkText = truncate(chunkText, targetWidth - curWidth)
+      local hlGroup = chunk[2]
+      table.insert(newVirtText, { chunkText, hlGroup })
+      chunkWidth = vim.fn.strdisplaywidth(chunkText)
+      -- str width returned from truncate() may less than 2nd argument, need padding
+      if curWidth + chunkWidth < targetWidth then
+        suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+      end
+      break
+    end
+    curWidth = curWidth + chunkWidth
+  end
+  table.insert(newVirtText, { suffix, "MoreMsg" })
+  return newVirtText
+end
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        event = { "BufReadPost", "BufNewFile" },
-        cmd = { "TSUpdateSync" },
-        opts = {
-            ensure_installed = { "tsx", "typescript", "html", "javascript", "bash", "css", "jsdoc", "json", "json5", "lua", "yaml" },
-            ignore_install = { "dart" },
-            autotag = {
-                enable = true,
-            },
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },
-            fold = { enable = true },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<CR>",
-                    node_incremental = "<CR>",
-                    node_decremental = "<BS>",
-                    scope_incremental = "<TAB>",
-                },
-            },
-
-        },
-        config = function(_, opts)
-            require("nvim-treesitter.configs").setup(opts)
-            vim.cmd([[set foldmethod=expr]])
-            vim.cmd([[set foldexpr=nvim_treesitter#foldexpr()]])
-            vim.cmd([[set nofoldenable]])
-            vim.opt.foldlevel = 99
-        end
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "kevinhwang91/nvim-ufo", -- for better syntax highlighting
+      "kevinhwang91/promise-async", -- for better syntax highlighting
     },
-    {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        event = { "BufReadPost", "BufNewFile" },
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                textobjects = {
-                    select = {
-                        enable = true,
-                        lookahead = true,
-                        keymaps = {
-                            ["af"] = "@function.outer",
-                            ["if"] = "@function.inner",
-                            ["ac"] = "@class.outer",
-                            ["ic"] = "@class.inner",
-                        },
-                    },
-                },
-            })
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = { "TSUpdateSync" },
+    opts = {
+      ensure_installed = {
+        "tsx",
+        "typescript",
+        "html",
+        "javascript",
+        "bash",
+        "css",
+        "jsdoc",
+        "json",
+        "json5",
+        "lua",
+        "yaml",
+      },
+      ignore_install = { "dart" },
+      autotag = {
+        enable = true,
+      },
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+      fold = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<CR>",
+          node_incremental = "<CR>",
+          node_decremental = "<BS>",
+          scope_incremental = "<TAB>",
+        },
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+      vim.cmd([[set foldmethod=expr]])
+      vim.cmd([[set foldexpr=nvim_treesitter#foldexpr()]])
+      vim.cmd([[set nofoldenable]])
+      vim.o.foldcolumn = "1" -- '0' is not bad
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+      require("ufo").setup({
+        provider_selector = function(bufnr, filetype, buftype)
+          return { "treesitter", "indent" }
         end,
-    },
-    {
-        'numToStr/Comment.nvim',
-        dependencies = {
-            'JoosepAlviste/nvim-ts-context-commentstring',
+        fold_virt_text_handler = handler,
+      })
+      vim.keymap.set("n", "zC", require("ufo").openAllFolds)
+      vim.keymap.set("n", "zO", require("ufo").closeAllFolds)
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+            },
+          },
         },
-        lazy = false,
-        config = function()
-            require('Comment').setup({
-                pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-            })
-        end
-    }
+      })
+    end,
+  },
+  {
+    "numToStr/Comment.nvim",
+    dependencies = {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+    },
+    lazy = false,
+    config = function()
+      require("Comment").setup({
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      })
+    end,
+  },
 }
